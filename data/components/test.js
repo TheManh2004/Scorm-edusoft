cabdd38624086fad16b1317dc7c2c6cd8e377b0b
Mạnh Nguyễn <themanh20004@gmail.com>
2025-02-04 22:37:30 +0700

    feat: update reactjs and antd in html

--- data/components/test.js
+++ data/components/test.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+const e = React.createElement;
+
+
+function App() {
+  const handleClick = () => {
+    alert('Bạn vừa nhấn nút!');
+  };
+
+  return e(
+    'div',
+    { className: 'container' },
+    e('h1', null, 'Chào mừng đến với React thuần!'),
+    e('button', { onClick: handleClick }, 'Nhấn tôi')
+  );
+}
+console.log(React.version);
+
+// Render component App vào phần tử có id="root"
+ReactDOM.render(e(App), document.getElementById('root'));
